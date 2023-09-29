dea471d214fafcdc3d46424e69ffe56dd25b62cc
Nathan Dulac <nathan.dulac@epitech.eu>
2023-09-29 15:49:39 +0200

    ajout animation gsap

--- js/animation.js
+++ js/animation.js
@@ -1,8 +1,69 @@
+//title animation
+gsap.from(".main__title", {
+    y: -50,
+    opacity: 0,
+    duration: 0.5,
+    delay: 0,
+    ease: "expo.out"
+})
 
-    document.addEventListener('DOMContentLoaded', function() {
-    var elementToAnimate = document.querySelector('.animate-me');
+//index.html animations
+gsap.from(".main__profil", {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    delay: 0,
+    ease: "expo.out"
+});
 
-    // Déclencher l'animation
-    elementToAnimate.style.opacity = 1;
-    elementToAnimate.style.top = '0px';
-    });
+gsap.from(".main__infos__presentation", {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    delay: 0.5,
+    ease: "expo.out"
+});
+
+//skills.html animations
+gsap.from(".main__competences", {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    delay: 0,
+    ease: "expo.out"
+});
+
+gsap.from(".main__formation", {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    delay: 0.5,
+    ease: "expo.out" });
+
+gsap.from(".main__professionnal-projects", {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    delay: 1,
+    ease: "expo.out" });
+
+//projects.html animations
+gsap.from(".main__projects", {
+    y: -50,
+    opacity: 0,
+    duration: 1,
+    delay: 0,
+    ease: "expo.out",
+    stagger: {
+        amount: 0.5
+    }
+});
+
+//contact.html animations
+gsap.from(".main__contact", {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    delay: 0,
+    ease: "expo.out"
+});
