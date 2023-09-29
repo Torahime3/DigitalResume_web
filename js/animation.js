cdf41a218978893453669aa4429c752aaeadfdac
Nathan Dulac <nathan.dulac@epitech.eu>
2023-09-29 14:51:23 +0200

    changes

---
+++ js/animation.js
@@ -0,0 +1,8 @@
+
+    document.addEventListener('DOMContentLoaded', function() {
+    var elementToAnimate = document.querySelector('.animate-me');
+
+    // Déclencher l'animation
+    elementToAnimate.style.opacity = 1;
+    elementToAnimate.style.top = '0px';
+    });
